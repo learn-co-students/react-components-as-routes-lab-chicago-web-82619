8c6c0cb9d26297d760beaf226f36d839765e318b
Joseph S Gansukh <g.sukhbat@gmail.com>
2020-06-14 21:28:37 -0500

    Done.

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -4,7 +4,21 @@ import { directors } from '../data';
 const Directors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Directors Page</h1>
+      {directors.map(dir => {
+        return (
+          <div>
+            <h2>{dir.name}</h2>
+            <ul>
+              {dir.movies.map(movie => {
+                return (
+                  <li>{movie}</li>
+                )
+              })}
+            </ul>
+          </div>
+        )
+      })}
     </div>
   );
 }
